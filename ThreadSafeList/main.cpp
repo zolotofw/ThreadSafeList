db1df077135a2b84fef22befa1231a97dfd8c57d
Alex Zoloto <zolotofw@gmail.com>
2023-08-17 21:31:12 +0300

    remove test_cases.hpp

--- ThreadSafeList/main.cpp
+++ ThreadSafeList/main.cpp
@@ -1,19 +1,10 @@
 
 #include <iostream>
-#include "test_cases.hpp"
 #include <crtdbg.h>
 #include "example_thread_safe_list.h"
-
-
 
 
 int main()
 {
 
-	//test_out_stream();
-	//test_push_front_and_back_if_empty_list();
-	//test_add_values();
-	//test_pop_front();
-	//test_pop_back();
-	//test_pop_front_back();
 }
